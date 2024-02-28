d6e8215926b7d4c9a16963f1a4b549e2f071f239
Debasish Bhola <devop69@gmail.com>
2024-02-28 21:55:21 +0530

    input fields added

--- bank/management/system/login.java
+++ bank/management/system/login.java
@@ -1,5 +1,87 @@
 package bank.management.system;
 
-public class login {
-    
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    JLabel label1,label2,label3;
+    JTextField textField2;
+    JPasswordField passwordField3;
+
+    Login(){
+        super("Bank Management System");
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png")); //Adding image from icon folder
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT); //Scaling the image
+
+        //To make scaled image visible on frame, but we cant directly show the image in the frame so we have to take the help of the JLabel
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+
+        //setting image dimensions
+        image.setBounds(350,10,100,100);
+        add(image);
+
+
+        //adding cards png
+        ImageIcon ii1 = new ImageIcon(ClassLoader.getSystemResource("icon/card.png"));
+        Image ii2 = ii1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon ii3 = new ImageIcon(ii2);
+        JLabel iimage = new JLabel(ii3);
+        iimage.setBounds(630,350,100,100);
+        add(iimage);
+
+        //Adding Text
+        label1 = new JLabel("WELCOME TO ATM");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("AvantGarde",Font.BOLD,38));
+        label1.setBounds(230,125,450,40);
+        add(label1);
+
+        label2 = new JLabel("Card No:");
+        label2.setFont(new Font("Ralway",Font.BOLD,28));
+        label2.setForeground(Color.WHITE);
+        label2.setBounds(150,190,375,30);
+        add(label2);
+        
+        //Text field
+        textField2 = new JTextField(15);
+        textField2.setBounds(325,190,230,30);
+        textField2.setFont(new Font("Arial",Font.BOLD,14));
+        add(textField2);
+
+        label3 = new JLabel("PIN:");
+        label3.setFont(new Font("Ralway",Font.BOLD,28));
+        label3.setForeground(Color.WHITE);
+        label3.setBounds(150,250,375,30);
+        add(label3);
+
+        //Password Field
+        passwordField3 = new JPasswordField(15);
+        passwordField3.setBounds(325,250,230,30);
+        passwordField3.setFont(new Font("Arial",Font.BOLD,14));
+        add(passwordField3);
+
+        //Adding background image
+        ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("icon/backbg.png"));
+        Image iii2 = iii1.getImage().getScaledInstance(850, 480, Image.SCALE_DEFAULT);
+        ImageIcon iii3 = new ImageIcon(iii2);
+        JLabel iiimage = new JLabel(iii3);
+        iiimage.setBounds(0,0,850,480);
+        add(iiimage);
+
+        //Setting Frame size and making the frame visible(It is hidden by default)
+        setLayout(null);
+        setSize(850,480);
+        setLocation(450,200);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        
+        new Login();
+    }
 }

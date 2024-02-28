462321d11c39c3e2e01e3a78687bad018326e1e3
Debasish Bhola <devop69@gmail.com>
2024-02-28 23:44:48 +0530

    random Application Num

--- bank/management/system/Signup.java
+++ bank/management/system/Signup.java
@@ -1,12 +1,33 @@
 package bank.management.system;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.util.Random;
 
 import javax.swing.*;
 
 public class Signup extends JFrame {
+    Random ran = new Random();
+    long first4 = (ran.nextLong() % 9000L) +1000L;
 
+    String first = " "+Math.abs(first4);
     Signup(){
+
+        super("APPLICATION FORM");
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(25,10,100,100);
+        add(image);
+
+        JLabel label1 = new JLabel("APPLICATION FORM NO."+first);
+        label1.setBounds(160,20,600,40);
+        label1.setFont (new Font("Rsleway",Font.BOLD,38));
+        add(label1);
+
         getContentPane().setBackground(new Color(222,255,228)); //Color Selection in RGB form
         setLayout(null);
         setSize(850,800);

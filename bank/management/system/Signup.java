21d95be5cf6f5648ccc073ae1212e03f93f15597
Debasish Bhola <devop69@gmail.com>
2024-02-29 08:02:34 +0530

    DateChooser added

--- bank/management/system/Signup.java
+++ bank/management/system/Signup.java
@@ -7,7 +7,14 @@ import java.util.Random;
 
 import javax.swing.*;
 
+import com.toedter.calendar.JDateChooser;
+
 public class Signup extends JFrame {
+    JTextField textName,textFname,textEmail,textAdd,textcity,textState,textPin;
+
+    // JDateChooser dateChooser;
+    JDateChooser dateChooser;
+    
     Random ran = new Random();
     long first4 = (ran.nextLong() % 9000L) +1000L;
 
@@ -27,6 +34,41 @@ public class Signup extends JFrame {
         label1.setBounds(160,20,600,40);
         label1.setFont (new Font("Rsleway",Font.BOLD,38));
         add(label1);
+
+        JLabel label2 = new JLabel("Page1");
+        label2.setFont(new Font("Ralway",Font.BOLD,22));
+        label2.setBounds(330,70,600,30);
+        add(label2);
+
+        JLabel label3 = new JLabel("Personal Details");
+        label3.setFont(new Font("Raleway", Font.BOLD,22));
+        label3.setBounds(290,90,600,30);
+        add(label3);
+
+        textName = new JTextField();
+        textName.setFont(new Font("Raleway",Font.BOLD, 14));
+        textName.setBounds(300,190,400,30);
+        add(textName);
+
+        JLabel labelName = new JLabel("Name :");
+        labelName.setFont(new Font("Raleway", Font.BOLD, 20));
+        labelName.setBounds(100,190,100,30);
+        add(labelName);
+
+        JLabel labelfName = new JLabel("Father's Name :");
+        labelfName.setFont(new Font("Raleway", Font.BOLD, 20));
+        labelfName.setBounds(100,240,200,30);
+        add(labelfName);
+
+        JLabel DOB = new JLabel("Date of Birth");
+        DOB.setFont(new Font("Raleway", Font.BOLD, 20));
+        DOB.setBounds(100,340,200,30);
+        add(DOB);
+
+        // dateChooser = new JDateChooser();
+        // dateChooser.setForeground(new Color(105,105,105));
+        // dateChooser.setBounds(300,340,400,30);
+        // add(dateChooser);
 
         getContentPane().setBackground(new Color(222,255,228)); //Color Selection in RGB form
         setLayout(null);

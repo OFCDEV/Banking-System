44dfe1421bae317da3d489418555a933109e22b2
Debasish Bhola <devop69@gmail.com>
2024-03-01 21:20:09 +0530

    Added DB code

---
+++ bank/management/system/databaseCode.sql
@@ -0,0 +1,4 @@
+create database bankSystem;
+use bankSystem;
+create table signup(form_no varchar(30),name varchar(30),father_name varchar(30),DOB varchar(30),gender varchar(30),email varchar(60),marital_status varchar(30),address varchar(60),city varchar(30),state varchar(30),pincode varchar(30));
+select * from signup;

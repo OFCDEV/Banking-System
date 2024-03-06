4dd9286bcb5544da5adb46c4964d32fb65ebef07
Debasish Bhola <devop69@gmail.com>
2024-03-06 23:21:12 +0530

    Bug Fix

--- bank/management/system/Signup3.java
+++ bank/management/system/Signup3.java
@@ -242,6 +242,7 @@ public class Signup3 extends JFrame implements ActionListener{
                     c1.statement.executeUpdate(q1);
                     c1.statement.executeUpdate(q2);
                     JOptionPane.showMessageDialog(null,"Card Number : "+cardno+"\n Pin : "+pin );
+                    new Deposite(pin);
                     setVisible(false);
                 }
             } else if (e.getSource()==c) {

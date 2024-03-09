06d510834d4a8276dc591aacfbc240cc99a197d7
Debasish Bhola <devop69@gmail.com>
2024-03-09 15:28:37 +0530

    Constructor added

--- bank/management/system/BalanceEnquriy.java
+++ bank/management/system/BalanceEnquriy.java
@@ -2,4 +2,11 @@ package bank.management.system;
 
 public class BalanceEnquriy {
 
+    BalanceEnquriy(){
+
+    }
+
+    public static void main(String[] args) {
+        new BalanceEnquriy();
+    }
 }
---
+++ bank/management/system/Pin.java
@@ -0,0 +1,5 @@
+package bank.management.system;
+
+public class Pin {
+
+}

90e53a6451a786829255720b1ace6c5164433d9b
Debasish Bhola <devop69@gmail.com>
2024-03-09 09:41:48 +0530

    Frame Added

--- bank/management/system/main_class.java
+++ bank/management/system/main_class.java
@@ -1,8 +1,26 @@
 package bank.management.system;
 
-public class main_class {
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class main_class extends JFrame{
+
     main_class(){
 
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
+        Image i2 = i1.getImage().getScaledInstance(1550,830,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel l3 = new JLabel(i3);
+        l3.setBounds(0,0,1550,830);
+        add(l3);
+
+        setLayout(null);
+        setSize(1550,1080);
+        setLocation(0,0);
+        setVisible(true);
     }
     public static void main(String[] args) {
         new main_class();
